d29befb6b311fe76d3e7ecb710363ed2a057fbb2
Jaemin Choi <jchoi157@illinois.edu>
2019-09-24 14:39:31 -0500

    Update config file with ChaNGa defaults

--- config.py
+++ config.py
@@ -8,20 +8,20 @@
 #
 
 # The GitHub repository to add the Pull Request to:
-GITHUB_REPO = 'github_repo'
+GITHUB_REPO = 'N-BodyShop/changa'
 
 # Pull requests will be created by this GitHub user if the real user is not
 # in the map below
-github_default_username = 'github_default_username'
+github_default_username = 'changa-import'
 
 # Maps Github user names to their Github access tokens. The tokens can be
 # generated in Settings -> Developer Settings -> Personal access tokens,
 # Generate new token. Only the public_repo scope is necessary.
 github_tokenmap = {
-  'github_user':  'github_token',
+  'changa-import':  '681cacbd8342cd77a379391e55bbf9be24cca03a',
 }
 
 # Maps Gerrit user names to Github user names:
 github_usermap = {
-  'Gerrit user':  'github_user',
+  'Gerrit user':  'GitHub user',
 }
